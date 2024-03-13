dc91cb3bbe996ce01b23b3207962badaa67e2be9
Xavi-Arnau <chavier28@gmail.com>
2024-03-13 19:28:09 +0100

    init

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,115 @@
+import React from "react";
+import proj1 from "../assets/proj1.png";
+import proj2 from "../assets/proj6.png";
+import proj3 from "../assets/proj3.png";
+import proj4 from "../assets/proj4.png";
+import { AiFillChrome, AiFillGithub } from "react-icons/ai";
+
+export const Portfolio = () => {
+  return (
+    <div className="pt-16 pb-10 sm:pt-16 sm:pb-16 lg:pb-16 md:h-[92vh]">
+      <div className="px-7 mx-auto grid sm:grid-cols-2 gap-6">
+        <div className="mb-4 flex flex-col sm:grid sm:grid-cols-8 rounded-lg bg-black/40 overflow-hidden shadow-xl z-1 sm:pr-5">
+          <a href="/" className="group h-48 col-span-3 md:h-80">
+            <img
+              src={proj1}
+              alt=""
+              className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110 rounded-lg"
+            />
+          </a>
+
+          <div className="flex flex-col col-span-4 p-4 mx-auto my-auto">
+            <h2 className="text-gray-200 font-bold text-xl leading-10">
+              Project 1
+            </h2>
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="flex flex-row justify-between text-white gap-4 mx-auto pb-4">
+            <AiFillGithub className="w-[55px] h-auto" />
+            <AiFillChrome className="w-[55px] h-auto" />
+          </div>
+        </div>
+        <div className="mb-4 flex flex-col sm:grid sm:grid-cols-8 rounded-lg bg-black/40 overflow-hidden shadow-xl z-1 sm:pr-5">
+          <a href="/" className="group h-48 col-span-3 md:h-80">
+            <img
+              alt=""
+              src={proj2}
+              className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110 rounded-lg"
+            />
+          </a>
+
+          <div className="flex flex-col col-span-4 p-4 mx-auto my-auto">
+            <h2 className="text-gray-200 font-bold text-xl leading-10">
+              Project 1
+            </h2>
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="flex flex-row justify-between text-white gap-4 mx-auto pb-4">
+            <AiFillGithub className="w-[55px] h-auto" />
+            <AiFillChrome className="w-[55px] h-auto" />
+          </div>
+        </div>
+        <div className="mb-4 flex flex-col sm:grid sm:grid-cols-8 rounded-lg bg-black/40 overflow-hidden shadow-xl z-1 sm:pr-5">
+          <a href="/" className="group h-48 col-span-3 md:h-80">
+            <img
+              alt=""
+              src={proj3}
+              className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110 rounded-lg"
+            />
+          </a>
+
+          <div className="flex flex-col col-span-4 p-4 mx-auto my-auto">
+            <h2 className="text-gray-200 font-bold text-xl leading-10">
+              Project 1
+            </h2>
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="flex flex-row justify-between text-white gap-4 mx-auto pb-4">
+            <AiFillGithub className="w-[55px] h-auto" />
+            <AiFillChrome className="w-[55px] h-auto" />
+          </div>
+        </div>
+        <div className="mb-4 flex flex-col sm:grid sm:grid-cols-8 rounded-lg bg-black/40 overflow-hidden shadow-xl z-1 sm:pr-5">
+          <a href="/" className="group h-48 col-span-3 md:h-80">
+            <img
+              alt=""
+              src={proj4}
+              className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110 rounded-lg"
+            />
+          </a>
+
+          <div className="flex flex-col col-span-4 p-4 mx-auto my-auto">
+            <h2 className="text-gray-200 font-bold text-xl leading-10">
+              Project 1
+            </h2>
+            <p className="text-gray-400">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+          <div className="flex flex-row justify-between text-white gap-4 mx-auto pb-4">
+            <AiFillGithub className="w-[55px] h-auto" />
+            <AiFillChrome className="w-[55px] h-auto" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
