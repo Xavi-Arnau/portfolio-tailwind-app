7469a7cda0e8b6e8be7e24c06e10b9bd2eedf8d9
Xavi-Arnau <chavier28@gmail.com>
2024-03-20 18:18:12 +0100

    added recipes project

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -129,6 +129,42 @@ export const Portfolio = () => {
         <div className="mb-4 flex flex-col sm:grid sm:grid-cols-8 rounded-lg bg-black/40 overflow-hidden shadow-xl z-1 sm:pr-5">
           <a
             target="blank_"
+            href="https://some-recipes-app.netlify.app/"
+            className="group h-48 col-span-3 md:h-80"
+          >
+            <img
+              src={proj1}
+              alt=""
+              className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110 rounded-lg"
+            />
+          </a>
+
+          <div className="flex flex-col col-span-4 p-4 mx-auto my-auto">
+            <h2 className="text-gray-200 font-bold text-xl leading-10">
+              Some recipes app
+            </h2>
+            <p className="text-gray-400">Recipes app using forkify API.</p>
+          </div>
+          <div className="flex flex-row justify-between text-white gap-4 mx-auto pb-4">
+            <a
+              target="blank_"
+              className="flex align-items"
+              href="https://github.com/Xavi-Arnau/recipe-app"
+            >
+              <AiFillGithub className="w-[55px] h-auto" />
+            </a>
+            <a
+              target="blank_"
+              className="flex align-items"
+              href="https://some-recipes-app.netlify.app/"
+            >
+              <AiFillChrome className="w-[55px] h-auto" />
+            </a>
+          </div>
+        </div>
+        <div className="mb-4 flex flex-col sm:grid sm:grid-cols-8 rounded-lg bg-black/40 overflow-hidden shadow-xl z-1 sm:pr-5">
+          <a
+            target="blank_"
             href="https://trivia-tailwind.netlify.app/"
             className="group h-48 col-span-3 md:h-80"
           >
